4f71a6d0832cc03afcfd084a5c60e676a1b582b0
Alexander <alexander.zaburdaev1904@gmail.com>
2023-11-16 23:14:57 +0300

    add vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    base: '/',
+    build: {
+        outDir: 'build'
+    },
+    plugins: [react(), viteTsconfigPaths()],
+    server: {
+        // this ensures that the browser opens upon server start
+        open: true,
+        port: 3000
+    }
+});
+
